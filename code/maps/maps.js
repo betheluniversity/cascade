0d38e749005f83c3211d2dd62dde970c4e1392c0
Peter St.Jean <pstjean@gmail.com>
2014-04-04 11:10:12 -0500

    Adding location selection

--- code/maps/maps.js
+++ code/maps/maps.js
@@ -1,7 +1,6 @@
 var geocoder;
 var map;
-var addressArray = new Array();
-var resultsArray = new Array();
+var markers = [];
 
 $(document).ready(function() {
     var $select = createMapSelect(locations);
@@ -20,14 +19,17 @@ function initialize(location, type) {
         geocoder = new google.maps.Geocoder();
         var latlng = new google.maps.LatLng(location.lat, location.long);
         var mapOptions = {
-            zoom: location.zoom,
+            //zoom: location.zoom,
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-        createBuildingSelect(location);
+        deleteMarkers();
+        createLocationSelect(location);
+        panSingleMap(location);
     } else if (type == "collection") {
+        deleteMarkers();
         makeCollectionMap(location);
         createLocationSelect(location);
     }
@@ -49,32 +51,99 @@ function createMapSelect(locations) {
     return $select;
 }
 
-function createBuildingSelect(campus) {
+function createLocationSelect(map) {
     clearSelects();
-    if(campus.hasOwnProperty('buildings')) {
+    if(map.hasOwnProperty('locations')) {
         var $select = $('<select />');
-        for(var building in campus.buildings) {
-            var buildingObj = campus.buildings[building];
-            var $option = $('<option />').val(building);
-            $option.text(buildingObj.label);
-            $select.append($option);
-        }
-        $('#bldg_select').html($select);
-    }
-}
-
-function createLocationSelect(collection) {
-    clearSelects();
-    if(collection.hasOwnProperty('locations')) {
-        var $select = $('<select />');
-        for(var locKey in collection.locations) {
-            var locationObj = collection.locations[locKey];
+        var $option = $('<option />').text('Select a Location');
+        $select.append($option);
+        for(var locKey in map.locations) {
+            var locationObj = map.locations[locKey];
             var $option = $('<option />').val(locKey);
             $option.text(locationObj.label);
             $select.append($option);
         }
         $('#loc_select').html($select);
+        $select.change(function() {
+            locationSelectChange(getCurrentlySelectedLocation($(this).val()))
+        });
     }
+}
+
+function getCurrentlySelectedLocation(location) {
+    var map = $('#map_select select').val();
+    var type = $('#map_select :selected').data('type');
+
+    var locationKeys = {
+        "type": type,
+        "map": map,
+        "location": location
+    }
+
+    return locationKeys;
+}
+
+function locationSelectChange(location) {
+    var mapObj = locations[location.type][location.map];
+    var locationObj = mapObj.locations[location.location];
+    var position = new google.maps.LatLng(locationObj.lat, locationObj.long);
+
+    if (location.type == 'single') {
+        deleteMarkers();
+        panSingleMap(mapObj);
+        addMarker(position, locationObj.label);
+    } else if (location.type == 'collection') {
+        if(map.zoom < 15) {
+            map.setZoom(17);
+        }
+
+        map.panTo(position);
+    }
+}
+
+function addMarker(location, label) {
+    var marker = new google.maps.Marker({
+        map:map,
+        animation: google.maps.Animation.DROP,
+        position: location,
+        title: label
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: marker.title,
+        position: location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+        map.setZoom(19);
+        map.panTo(location);
+
+    });
+
+    markers.push(marker);
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setAllMap(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
 }
 
 function clearSelects() {
@@ -82,51 +151,29 @@ function clearSelects() {
     $('#loc_select').html('');
 }
 
-// function makeLocationMap(locations) {
-//     for(campus in locations.single) {
-//         var campus = locations.single[campus];
-//         console.log(campus.lat);
-//         console.log(campus.long);
-//         console.log(campus.label);
-//         console.log(campus.address);
-//         for(building in campus.buildings) {
-//             var building = campus.buildings[building];
-//             console.log(building.label);
-//         }
-//     }
-// }
+// Fitting a single map to the locations defined in it
+function panSingleMap(single)
+{
+    var LocationBounds = new Object();
+    LocationBounds.latitude = -1;
+    LocationBounds.longitude = -1;
 
-// function makeCollectionMap(geoArray) {
-//     var LocationBounds = new Object();
-//     LocationBounds.latitude = -1;
-//     LocationBounds.longitude = -1;
-//
-//     bounds = new google.maps.LatLngBounds();
-//
-//     for (var x = 0; x < geoArray.length; x++)
-//     {
-//
-//         var marker = new google.maps.Marker({
-//             map: map,
-//             position: results[0].geometry.location//,
-//             //icon: icon
-//         });
-//         // adding an event listener
-//         // this has to be done for each marker independently
-//         google.maps.event.addListener(marker, 'click', function() {
-//
-//         });
-//
-//         bounds.extend(results[0].geometry.location);
-//         if (x === geoArray.length - 1) {
-//             map.fitBounds(bounds);
-//             // TODO check if this is a single job map
-//             map.setZoom(16);
-//         }
-//
-//     }
-//     map.panToBounds(bounds);
-// }
+    bounds = new google.maps.LatLngBounds();
+
+    // loop through the collection and add
+    for (locKey in single.locations) {
+        var locationObj = single.locations[locKey];
+        var lat = locationObj.lat;
+        var long = locationObj.long;
+
+        var locationLatlng = new google.maps.LatLng(lat,long);
+        bounds.extend(locationLatlng);
+
+        map.fitBounds(bounds);
+    }
+
+    map.panToBounds(bounds);
+}
 
 function makeCollectionMap(collection)
 {
@@ -144,17 +191,7 @@ function makeCollectionMap(collection)
 
         var locationLatlng = new google.maps.LatLng(lat,long);
 
-        var marker = new google.maps.Marker({
-            position: locationLatlng,
-            map: map,
-            title: locationObj.label
-        });
-
-        // adding an event listener
-        // this has to be done for each marker independently
-        google.maps.event.addListener(marker, 'click', function() {
-
-        });
+        addMarker(locationLatlng, locationObj.label);
 
         bounds.extend(locationLatlng);
 
