ed006a596de2b1c5aae2c5c7943cce7b39174c76
c-schwarze <ces55739@bethel.edu>
2016-06-07 16:12:59 -0500

    updated the version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ file_text = read(fpath('bu_cascade/__init__.py'))
 
 setup(
     name='bu_cascade',
-    version=2.06,
+    version=2.07,
     description='Cascade Server web services integration',
     long_description='',
     url='https://github.com/betheluniversity/cascade/',
