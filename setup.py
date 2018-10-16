5ceb24deaa6b61e3e94c36007aaae4328765d2d1
c-schwarze <ces55739@bethel.edu>
2018-10-16 16:23:25 -0500

    apparently we can't do 3.01 - changed to 3.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ file_text = read(fpath('bu_cascade/__init__.py'))
 
 setup(
     name='bu_cascade',
-    version=3.01,
+    version=3.1,
     description='Cascade Server web services integration',
     long_description='',
     url='https://github.com/betheluniversity/cascade/',
