dfea6e709af4b58022f1234911d8df92712f5e3e
c-schwarze <ces55739@bethel.edu>
2018-10-17 09:47:25 -0500

    Updated to 3.11 since it won't show up on pypi

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ file_text = read(fpath('bu_cascade/__init__.py'))
 
 setup(
     name='bu_cascade',
-    version=3.1,
+    version=3.11,
     description='Cascade Server web services integration',
     long_description='',
     url='https://github.com/betheluniversity/cascade/',
